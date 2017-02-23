b5afe64da4b3782599ec39c92d344233cf323092
Thomas Huisking <tomasfaustin@gmail.com>
2017-02-22 16:44:13 -0800

    Add heroku and bootstrap shit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cupcake.create({
+  :flavor => "Chocolate Chip",
+  :quantity => "10",
+  :price => "2.00"
+  })
